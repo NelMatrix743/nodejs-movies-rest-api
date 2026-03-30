37c7284b6f763ee538fc510a6e37d51f8452b189
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-30 14:48:13 +0000

    slight changes

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ import app from "./src/app.js";
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(
+    `Movies REST API system listening on port ${port}\nURL: http://localhost:3000/`
+  );
 });
+
+// EOSC
--- src/app.js
+++ src/app.js
@@ -3,13 +3,15 @@ import express from 'express';
 
 const app = express();
 
-const RESPONSE = `
+const BASE_RESPONSE = `
 	<h1 style='font-family:consolas; font-size: 100px;'>Hello, World!</h1>
 `;
 
 app.get('/', (req, res) => {
-  res.send(RESPONSE)
+  res.send(BASE_RESPONSE);
 });
 
 
 export default app;
+
+// EOSC
