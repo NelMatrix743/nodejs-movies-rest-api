7198437b0f26e5517ef811f29e936c90dd8109c5
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-30 18:08:46 +0000

    set CORS and HTTP logging settings for improved DX

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-import express from 'express';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 
 const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 const BASE_RESPONSE = `
 	<h1 style='font-family:consolas; font-size: 100px;'>Hello, World!</h1>
