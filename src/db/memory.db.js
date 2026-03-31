314281e9a728226f195d7e4a22cbbb8fc4a26863
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-31 12:56:08 +0000

    feat(data): implement method for updating existing movie metadata

--- src/db/memory.db.js
+++ src/db/memory.db.js
@@ -49,6 +49,19 @@ export const createMovie = (movieData) => {
   return { id: newId, ...movies[newId] };
 };
 
+// Update movie
+export const updateMovie = (id, updatedData) => {
+  if (!movies[id]) return null;
+
+  movies[id] = {
+    ...movies[id],
+    ...updatedData,
+  };
+
+  writeData(movies);
+  return { id, ...movies[id] };
+};
+
 
 
 // ESOC
