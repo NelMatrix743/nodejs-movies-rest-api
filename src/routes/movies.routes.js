82d07ce02ffb09e1e9819dd6402ae5be98659f95
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-31 15:53:27 +0000

    feat(logic): implement create and update validation middleware in controller

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,12 +1,25 @@
 import express from "express";
 import * as controller from "../controllers/movies.controller.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import {
+  createMovieSchema,
+  updateMovieSchema,
+} from "../validations/movies.validation.js";
+
 
 const router = express.Router();
 
+// GET Requests
 router.get("/", controller.getMovies);
 router.get("/:id", controller.getMovie);
-router.post("/", controller.createMovie);
-router.put("/:id", controller.updateMovie);
+
+// POST Requests (apply validation middleware)
+router.post("/", validate(createMovieSchema), controller.createMovie);
+
+// UPDATE Requests (apply validation middleware)
+router.put("/:id", validate(updateMovieSchema), controller.updateMovie);
+
+// DELETE Requests
 router.delete("/:id", controller.deleteMovie);
 
 export default router;
