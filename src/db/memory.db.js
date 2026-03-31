a25a46f25b1367aff655b605149e4641cb0b2ce3
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-31 12:53:42 +0000

    feat(data): implement movie id generation function

--- src/db/memory.db.js
+++ src/db/memory.db.js
@@ -10,5 +10,15 @@ export const initDB = () => {
 };
 
 
+// Generate next ID safely
+const generateId = () => {
+  const ids = Object.keys(movies).map(Number);
+  if (ids.length === 0) return "0";
+
+  const maxId = Math.max(...ids);
+  return String(maxId + 1);
+};
+
+
 
 // ESOC
