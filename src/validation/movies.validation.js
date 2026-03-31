d822321eed70f3e928636ca33cab54060f5f6d70
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-31 15:41:10 +0000

    feat(logic): implement validation schema for creating new movie metadata

---
+++ src/validation/movies.validation.js
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+
+
+export const createMovieSchema = Joi.object({
+  title: Joi.string().min(1).required(),
+
+  release_year: Joi.number().integer().min(1800).max(2100).required(),
+
+  age_certification: Joi.string().allow("").optional(),
+
+  runtime: Joi.number().integer().min(1).required(),
+
+  genres: Joi.array().items(Joi.string()).required(),
+
+  production_countries: Joi.array().items(Joi.string()).required(),
+
+  seasons: Joi.string().allow("").optional(),
+
+  imdb_score: Joi.number().min(0).max(10).optional(),
+
+  imdb_votes: Joi.number().integer().min(0).optional(),
+});
+
+
+
+// EOSC
