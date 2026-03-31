c830b213c56300910276e431990034ec412c122b
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-31 13:05:47 +0000

    feat(logic): define endpoints for the movie route

---
+++ src/routes/movies.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import * as controller from "../controllers/movie.controller.js";
+
+const router = express.Router();
+
+router.get("/", controller.getMovies);
+router.get("/:id", controller.getMovie);
+router.post("/", controller.createMovie);
+router.put("/:id", controller.updateMovie);
+router.delete("/:id", controller.deleteMovie);
+
+export default router;
+
+
+// EOSC
