533762123b3fd0481ef70b0f5386e5aaa1ff60ea
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-31 12:55:20 +0000

    feat(data): implement method for creating new movie metadata

--- src/db/memory.db.js
+++ src/db/memory.db.js
@@ -29,5 +29,26 @@ export const getMovieById = (id) => {
 };
 
 
+// Create a new movie
+export const createMovie = (movieData) => {
+  const newId = generateId();
+
+  movies[newId] = {
+    title: movieData.title,
+    release_year: movieData.release_year,
+    age_certification: movieData.age_certification,
+    runtime: movieData.runtime,
+    genres: movieData.genres,
+    production_countries: movieData.production_countries,
+    seasons: movieData.seasons,
+    imdb_score: movieData.imdb_score,
+    imdb_votes: movieData.imdb_votes,
+  };
+
+  writeData(movies);
+  return { id: newId, ...movies[newId] };
+};
+
+
 
 // ESOC
